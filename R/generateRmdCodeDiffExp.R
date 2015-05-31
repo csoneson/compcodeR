eec610304f3043caea73afbf51babad7c060c795
Charlotte Soneson <charlottesoneson@gmail.com>
2015-05-31 14:15:48 +0200

    Updated baySeq code to comply with package version 2.2.0

--- R/generateRmdCodeDiffExp.R
+++ R/generateRmdCodeDiffExp.R
@@ -550,7 +550,6 @@ voom.limma.createRmd <- function(data.path, result.path, codefile, norm.method) 
 #' @param result.path The path to the file where the result object will be saved.
 #' @param codefile The path to the file where the code will be written.
 #' @param norm.method The between-sample normalization method used to compensate for varying library sizes and composition in the differential expression analysis. Possible values are \code{"quantile"}, \code{"total"} and \code{"edgeR"}. 
-#' @param distr.choice The choice of underlying distribution assumption for the observed counts. Possible values are \code{"NB"}, \code{"Poisson"} and \code{"Dirichlet"}. 
 #' @param equaldisp Logical parameter indicating whether or not equal dispersion should be assumed across all conditions.
 #' @param sample.size The size of the sample used to estimate the priors (default 5000).
 #' @param estimation The approach used to estimate the priors. Possible values are \code{"QL"} (default), \code{"ML"} and \code{"edgeR"}.
@@ -574,12 +573,11 @@ voom.limma.createRmd <- function(data.path, result.path, codefile, norm.method) 
 #' runDiffExp(data.file = file.path(tmpdir, "mydata.rds"), result.extent = "baySeq", 
 #'            Rmdfunction = "baySeq.createRmd", 
 #'            output.directory = tmpdir, norm.method = "edgeR",
-#'            distr.choice = "NB", equaldisp = TRUE, 
-#'            sample.size = 10)
+#'            equaldisp = TRUE, sample.size = 10)
 #' })
 baySeq.createRmd <- function(data.path, result.path, codefile, 
-                                norm.method, distr.choice, equaldisp, sample.size = 5000, 
-                                estimation = "QL", pET = "BIC") {
+                             norm.method, equaldisp, sample.size = 5000, 
+                             estimation = "QL", pET = "BIC") {
   codefile <- file(codefile, open = 'w')
   writeLines("### baySeq", codefile)
   writeLines(paste("Data file: ", data.path, sep = ''), codefile)
@@ -593,17 +591,9 @@ baySeq.createRmd <- function(data.path, result.path, codefile,
   writeLines(c("is.valid <- check_compData(cdata)",
                "if (!(is.valid == TRUE)) stop('Not a valid compData object.')",
                "baySeq.cd <- new('countData', data = count.matrix(cdata), replicates = sample.annotations(cdata)$condition, groups = list(NDE = rep(1, length(sample.annotations(cdata)$condition)), DE = sample.annotations(cdata)$condition))", 
-               paste("baySeq.cd@libsizes <- baySeq::getLibsizes(baySeq.cd, estimationType = '", norm.method, "')", sep = '')), codefile)
-  if (distr.choice == 'NB') {
-    writeLines(c(paste("baySeq.cd <- baySeq::getPriors.NB(baySeq.cd, samplesize =", sample.size, ", equalDispersions = ", equaldisp, ", estimation = '", estimation, "', cl = NULL)", sep = ''), 
-                 paste("baySeq.cd <- baySeq::getLikelihoods.NB(baySeq.cd, prs = c(0.5, 0.5), pET = '", pET, "', cl = NULL)", sep = '')), codefile)
-  } else if (distr.choice == 'Poisson') {
-    writeLines(c(paste("baySeq.cd <- baySeq::getPriors.Pois(baySeq.cd, samplesize =", sample.size, ", cl = NULL)", sep = ''), 
-                 paste("baySeq.cd <- baySeq::getLikelihoods.Pois(baySeq.cd, prs = c(0.5, 0.5), pET = '", pET, "', cl = NULL)", sep = '')), codefile)
-  } else if (distr.choice == 'Dirichlet') {
-    writeLines(c(paste("baySeq.cd <- baySeq::getPriors.Dirichlet(baySeq.cd, samplesize =", sample.size, ", cl = NULL)", sep = ''), 
-                 paste("baySeq.cd <- baySeq::getLikelihoods.Dirichlet(baySeq.cd, prs = c(0.5, 0.5), pET = '", pET, "', cl = NULL)", sep = '')), codefile)
-  }
+               paste("libsizes(baySeq.cd) <- baySeq::getLibsizes(baySeq.cd, estimationType = '", norm.method, "')", sep = '')), codefile)
+  writeLines(c(paste("baySeq.cd <- baySeq::getPriors.NB(baySeq.cd, samplesize =", sample.size, ", equalDispersions = ", equaldisp, ", estimation = '", estimation, "', cl = NULL)", sep = ''), 
+                 paste("baySeq.cd <- baySeq::getLikelihoods(baySeq.cd, prs = c(0.5, 0.5), pET = '", pET, "', cl = NULL)", sep = '')), codefile)
   writeLines(c("baySeq.posteriors.DE <- exp(baySeq.cd@posteriors)[, 'DE']", 
                "baySeq.FDR <- baySeq::topCounts(baySeq.cd, group = 'DE', FDR = 1)$FDR.DE[match(rownames(count.matrix(cdata)), rownames(baySeq::topCounts(baySeq.cd, group = 'DE', FDR = 1)))]", 
                "baySeq.score <- 1 - baySeq.FDR", 
@@ -612,7 +602,7 @@ baySeq.createRmd <- function(data.path, result.path, codefile,
                "result.table(cdata) <- result.table", 
                "package.version(cdata) <- paste('baySeq,', packageVersion('baySeq'))",
                "analysis.date(cdata) <- date()",
-               paste("method.names(cdata) <- list('short.name' = 'baySeq', 'full.name' = '", paste('baySeq.', packageVersion('baySeq'), '.', norm.method, '.', distr.choice, '.', ifelse(equaldisp == TRUE, 'equaldisp', 'nonequaldisp'), '.samplesize', sample.size, '.', estimation, '.', pET, sep = ''), "')", sep = ''),
+               paste("method.names(cdata) <- list('short.name' = 'baySeq', 'full.name' = '", paste('baySeq.', packageVersion('baySeq'), '.', norm.method, '.', ifelse(equaldisp == TRUE, 'equaldisp', 'nonequaldisp'), '.samplesize', sample.size, '.', estimation, '.', pET, sep = ''), "')", sep = ''),
                "is.valid <- check_compData_results(cdata)",
                "if (!(is.valid == TRUE)) stop('Not a valid compData result object.')",
                paste("saveRDS(cdata, '", result.path, "')", sep = "")), codefile)
