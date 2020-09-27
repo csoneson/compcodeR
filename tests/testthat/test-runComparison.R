329c9c956964ab78a62b356368b2f0ad8dec431c
Charlotte Soneson <charlottesoneson@gmail.com>
2020-09-27 11:20:24 +0200

    Add tests

--- tests/testthat/test-runComparison.R
+++ tests/testthat/test-runComparison.R
@@ -27,7 +27,49 @@ test_that("generateSyntheticData fails with wrong inputs", {
     dispersions = 1:3,
     output.file = file.path(tdir, "tmp.rds")
   ), "The number of provided dispersions must be the same as the number of simulated genes.")
+})
+
+test_that("compData object checks work", {
+  tmp <- generateSyntheticData(
+    dataset = "B_625_625", n.vars = 50, 
+    samples.per.cond = 5, n.diffexp = 0, 
+    output.file = NULL
+  )
+  tmp
   
+  expect_equal(checkDataObject(tmp), "Data object looks ok.")
+  
+  l <- convertcompDataToList(tmp)
+  expect_is(l, "list")
+  cpd <- convertListTocompData(l)
+  expect_is(cpd, "compData")
+  expect_equal(checkDataObject(cpd), "Data object looks ok.")
+  l$count.matrix <- NULL
+  expect_message({cpd2 <- convertListTocompData(l)}, 
+                 "Cannot convert list to compData object")
+  expect_equal(cpd2, NULL)
+  
+  tmp2 <- tmp; expect_error({count.matrix(tmp2) <- 1:3})
+  tmp2 <- tmp; expect_error({sample.annotations(tmp2) <- 1:3})
+  tmp2 <- tmp; expect_error({filtering(tmp2) <- 1:3})
+  tmp2 <- tmp; expect_error({analysis.date(tmp2) <- 1:3})
+  tmp2 <- tmp; expect_error({package.version(tmp2) <- 1:3})
+  tmp2 <- tmp; expect_error({info.parameters(tmp2) <- 1:3})
+  
+  expect_equal(check_compData(tmp), TRUE)
+  expect_equal(check_compData(tmp@count.matrix), "This is not an S4 object.")
+  tmp2 <- tmp; tmp2@count.matrix <- as.matrix(numeric(0)); expect_equal(check_compData(tmp2), "Object must contain a non-empty count matrix.")
+  tmp2 <- tmp; tmp2@sample.annotations <- as.data.frame(NULL); expect_equal(check_compData(tmp2), "Object must contain a non-empty sample annotation data frame.")
+  tmp2 <- tmp; tmp2@sample.annotations <- data.frame(condition = numeric(0)); expect_equal(check_compData(tmp2), "The sample.annotations must contain a column named condition.")
+  tmp2 <- tmp; tmp2@info.parameters <- list(); expect_equal(check_compData(tmp2), "Object must contain a non-empty list called info.parameters.")
+  tmp2 <- tmp; tmp2@info.parameters <- list(tmp = 1); expect_equal(check_compData(tmp2), "info.parameters list must contain an entry named 'dataset'.")
+  tmp2 <- tmp; tmp2@info.parameters <- list(dataset = ""); expect_equal(check_compData(tmp2), "info.parameters list must contain an entry named 'uID'.")
+  tmp2 <- tmp; tmp2@count.matrix <- tmp2@count.matrix[1:10, ]; expect_equal(check_compData(tmp2), "count.matrix and variable.annotations do not contain the same number of rows.")
+  tmp2 <- tmp; rownames(tmp2@count.matrix) <- paste0("r", rownames(tmp2@count.matrix)); expect_equal(check_compData(tmp2), "The rownames of count.matrix and variable.annotations are not the same.")
+  tmp2 <- tmp; tmp2@count.matrix <- tmp2@count.matrix[, 1:2]; expect_equal(check_compData(tmp2), "The number of columns of count.matrix is different from the number of rows of sample.annotations.")
+  tmp2 <- tmp; colnames(tmp2@count.matrix) <- paste0("r", colnames(tmp2@count.matrix)); expect_equal(check_compData(tmp2), "The colnames of count.matrix are different from the rownames of sample.annotations.")
+  
+  expect_equal(check_compData_results(tmp), "Object must contain a list named 'method.names' identifying the differential expression method used.")
 })
 
 test_that("generateSyntheticData works", {
@@ -106,6 +148,20 @@ test_that("generateSyntheticData works", {
   expect_equal(sum(tmp@variable.annotations$upregulation + 
                      tmp@variable.annotations$downregulation), 10)
   
+  ## Not overdispersed
+  tmp <- generateSyntheticData(
+    dataset = "B_625_625", n.vars = 50, 
+    samples.per.cond = 5, n.diffexp = 10,
+    between.group.diffdisp = FALSE, 
+    fraction.non.overdispersed = 0.5,
+    output.file = NULL
+  )
+  expect_is(tmp, "compData")
+  expect_equal(tmp@variable.annotations$truedispersions.S1,  
+               tmp@variable.annotations$truedispersions.S2)
+  expect_equal(any(tmp@variable.annotations$truedispersions.S1 == 0), TRUE)
+  expect_equal(tmp@info.parameters$fraction.non.overdispersed, 0.5)
+
   ## Outliers
   set.seed(1)
   tmp <- generateSyntheticData(
@@ -134,10 +190,21 @@ test_that("generateSyntheticData works", {
 })
 
 test_that("help functions work", {
+  listcreateRmd()
+  
   tmp <- generateSyntheticData(
     dataset = "B_625_625", n.vars = 50, 
     samples.per.cond = 5, n.diffexp = 10,
     output.file = NULL
+  )
+  
+  expect_error(
+    computeMval(tmp@count.matrix, c(3, tmp@sample.annotations$condition[-1])),
+    "Must have exactly two groups to calculate M-value"
+  )
+  expect_error(
+    computeAval(tmp@count.matrix, c(3, tmp@sample.annotations$condition[-1])),
+    "Must have exactly two groups to calculate A-value"
   )
   
   mval <- computeMval(tmp@count.matrix, tmp@sample.annotations$condition)
@@ -165,6 +232,8 @@ test_that("runDiffExp works", {
     fraction.non.overdispersed = 0, 
     output.file = file.path(tdir, "B_625_625_5spc_repl1.rds")
   )
+  
+  expect_equal(checkDataObject(testdat), "Data object looks ok.")
   
   tmp <- readRDS(file.path(tdir, "B_625_625_5spc_repl1.rds"))
   expect_is(tmp, "compData")
@@ -315,6 +384,10 @@ test_that("runDiffExp works", {
     expect_is(tmp@package.version, "character")
     expect_is(tmp@method.names, "list")
     expect_named(tmp@method.names, c("short.name", "full.name"))
+    
+    tmp2 <- tmp; result.table(tmp2) <- result.table(tmp2)[1:10, ]; expect_equal(check_compData_results(tmp2), "result.table must have the same number of rows as count.matrix.")
+    tmp2 <- tmp; result.table(tmp2) <- data.frame(); expect_equal(check_compData_results(tmp2), "Object must contain a data frame named 'result.table'.")
+    tmp2 <- tmp; result.table(tmp2)$score <- NULL; expect_equal(check_compData_results(tmp2), "result.table must contain a column named 'score'.")
   }
   
   for (m in methods) {
