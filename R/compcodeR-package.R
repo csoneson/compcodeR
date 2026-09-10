decbcc38b279dbec585627721f06c9f225053e53
Charlotte Soneson <charlottesoneson@gmail.com>
2026-09-10 10:18:26 +0200

    Switch modeest dependency for statip

--- R/compcodeR-package.R
+++ R/compcodeR-package.R
@@ -7,10 +7,8 @@
 #' interfaces to several packages for performing the differential expression analysis.
 #' @name compcodeR-package
 #' @aliases compcodeR
-#' @docType package
-#' @import knitr markdown ROCR lattice gplots gtools caTools grid KernSmooth MASS ggplot2 stringr modeest limma edgeR vioplot methods rmarkdown sm
-#' @title RNAseq data simulation, differential expression analysis and
-#' performance comparison of differential expression methods
+#' @import knitr markdown ROCR lattice gplots gtools caTools grid KernSmooth MASS ggplot2 stringr limma edgeR vioplot methods rmarkdown sm
+#' @importFrom statip mfv
 #' @author Charlotte Soneson
-#' @keywords package
-NULL
+#' @keywords internal
+"_PACKAGE"
