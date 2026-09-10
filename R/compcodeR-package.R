61dcdc49d2a309c5c72fbd6e175aada1a8cd0e5b
Charlotte Soneson <charlottesoneson@gmail.com>
2026-09-10 10:12:44 +0200

    Update Roxygen

--- R/compcodeR-package.R
+++ R/compcodeR-package.R
@@ -7,11 +7,8 @@
 #' interfaces to several packages for performing the differential expression analysis.
 #' @name compcodeR-package
 #' @aliases compcodeR
-#' @docType package
-#' @title RNAseq data simulation, differential expression analysis and
-#' performance comparison of differential expression methods
 #' @import knitr markdown ROCR lattice gplots gtools caTools grid KernSmooth MASS ggplot2 stringr limma edgeR vioplot methods rmarkdown sm
 #' @importFrom statip mfv
 #' @author Charlotte Soneson
-#' @keywords package
-NULL
+#' @keywords internal
+"_PACKAGE"
